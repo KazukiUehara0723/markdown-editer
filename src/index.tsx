8921cfad97d522f45c0e32754acd5977901f8883
KazukiUehara0723 <ueharakazuki@pro.headwaters.co.jp>
2024-06-26 11:07:04 +0900

    second Commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const Main = (
+	<h1>Markdown</h1>
+);
+
+render(Main,document.getElementById('app'));
+
+// const log = (message:string):void => {
+//     console.log(message);
+// }
+
+// log("Hello,Webpock + TypeScript!")
